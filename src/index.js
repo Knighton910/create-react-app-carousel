0ce5dc0ee28765a8d11d695ded6534b045ade2c5
kelvin knigton <keltheceo@gmail.com>
2017-03-16 14:20:53 -0700

    1st awesome commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SimpleSlider from './App';
+import './index.css';
+
+ReactDOM.render(
+  <SimpleSlider />,
+  document.getElementById('root')
+);
